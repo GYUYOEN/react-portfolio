0718cb6cfb49a682168344e89e7a2544a6e53850
김규연 <rbdus9696@naver.com>
2023-09-19 03:03:47 +0900

    react-portfolio

---
+++ src/components/main.js
@@ -0,0 +1,19 @@
+import './main.css';
+import Heart from './heart.png';
+
+function Main() {
+    return (
+    <div>
+      <div className="main">
+        <div className="my-photo">
+          <p className="title-welcome">Welcome:)</p>
+          <p className="title-name">GyuYeon</p>
+          <p className="title-developer">DEVELOPER</p>
+          <p className="title-portfolio">portfolio<img className="heart" src={Heart} alt="heart"></img></p>
+        </div>
+      </div>
+    </div>
+    );
+}
+
+export default Main;
---
+++ src/components/navbar.js
@@ -0,0 +1,29 @@
+import './navbar.css';
+
+function Navbar() {
+    return (
+        <header>
+            <nav className="navbar">
+                <div className="brand">
+                    <a className="url" href="#!">about GueYoen</a>
+                </div>
+                <ul className="nav-link">
+                    <li>
+                        <a className="url" href="#!">project</a>
+                    </li>
+                    <li>
+                        <a className="url" href="#!">skills</a>
+                    </li>
+                    <li>
+                        <a className="url" href="#!">share</a>
+                    </li>
+                </ul>
+                <a className="navbar__toggleBtn" href="#!">
+                    <i className="fas fa-bars"></i>
+                </a>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
