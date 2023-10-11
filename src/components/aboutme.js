ebb5442cc155b7cd82698c94b51a6716cec56982
김규연 <rbdus9696@naver.com>
2023-10-12 02:00:41 +0900

    more about me 링크 추가

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -30,6 +30,7 @@ function AboutMe() {
                     <p className='aboutme-text'>거주지 : 경기도 안산시 상록구</p>
                     <p className='aboutme-text'>지원분야 : 백앤드 개발자</p>
                     <p className='aboutme-text'>한줄소개 : 성실한 신입 백앤드 개발자로서 열심히 노력 중입니다.<br/> 함께 성장하고 배우는 즐거움을 공유하고 싶습니다. 😊👨‍💻</p>
+                    <p className='more-about-me'><a href="https://bolder-sousaphone-710.notion.site/d9834112174d4118a778e2be8978b36e?pvs=4">More About Me</a></p>
                 </div>
             </div>
         </section>
